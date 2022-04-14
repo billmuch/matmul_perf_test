f6cf097605b32e3645974356ac0401d02aaa2257
billmuch <7769521@qq.com>
2022-04-14 16:55:08 +0800

    remove -g

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-clang++ blis.cpp -DFLOAT32 -fopenmp -g -O2 -o blis_float32.x -I/home/neal/local/include/blis -L/home/neal/local/lib -lblis 
-clang++ blis.cpp -DFLOAT64 -fopenmp -g -O2 -o blis_float64.x -I/home/neal/local/include/blis -L/home/neal/local/lib -lblis 
+clang++ blis.cpp -DFLOAT32 -fopenmp -O2 -o blis_float32.x -I/home/neal/local/include/blis -L/home/neal/local/lib -lblis 
+clang++ blis.cpp -DFLOAT64 -fopenmp -O2 -o blis_float64.x -I/home/neal/local/include/blis -L/home/neal/local/lib -lblis 
